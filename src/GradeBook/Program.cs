d8c9433a6cf43363c2a4088249049e039299ab16
Azher <woldome@gmail.com>
2019-11-21 10:58:18 +0300

    Basic program built

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string [] args)
         {
-            Console.WriteLine("Hello World! bouiiiiiii");
+            List<Double> grades = new List<Double>{30,10,50.1};
+            GradeBook gradeBook = new GradeBook(grades);
+            gradeBook.getStatisticas();
+        
+        }
+    }
+    class GradeBook
+    {
+        private readonly List<Double> _gradeList;
+        public GradeBook(List<Double> gradeList)
+        {
+            _gradeList=gradeList;
+        }
+       
+        public List<double> getGradeList()
+        {
+            return _gradeList;
+        }
+
+        public double getAverage()
+        {
+            double sum =0;
+            foreach(var item in _gradeList) sum+=item;
+            return  sum/_gradeList.Count;
+        }
+        public double getMax()
+        {
+            double tmp = double.MinValue;
+            foreach(var item in _gradeList) {if(item >tmp) tmp=item;}
+            return tmp;
+        }
+        public double getMin()
+        {
+            double tmp = double.MaxValue;
+            foreach(var item in _gradeList) {if(item <tmp) tmp=item;}
+            return tmp;
+        }
+        public void printList()
+        {
+            foreach(var item in _gradeList) Console.Write(item+" ");
+            Console.WriteLine();
+        }
+        public void getStatisticas()
+        {
+            Console.WriteLine("the grades are");
+            printList();
+            
+            Console.WriteLine("highest grade is: "+getMax());
+            Console.WriteLine("lowest grade is: "+getMin());
+            Console.WriteLine("average of grades is: "+getAverage());
         }
     }
 }
